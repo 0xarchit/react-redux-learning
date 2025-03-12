ca90c02a38f9800ff3766b50b050a636898a42ab
Archit Jain <0xarchit@gmail.com>
2025-03-12 19:00:55 +0530

    learnt useReducer

---
+++ App1/Projects/todo-list-vreducer/src/components/TodoItem copy.jsx
@@ -0,0 +1,19 @@
+function TodoItem() {
+    let todoName = 'Milk'
+    let todoDate = '12/12/12'
+    return <div className="container">
+        <div className="row">
+          <div className="col-6">
+            {todoName}
+          </div>
+          <div className="col-4">
+            {todoDate}
+          </div>
+          <div className="col-2">
+            <button className="btn btn-danger btn-prop">Delete</button>
+          </div>
+        </div>        
+      </div>
+}
+
+export default TodoItem;
