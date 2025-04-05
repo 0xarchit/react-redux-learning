b4d59e8a57b064fea7a55d7fb2c28b89f038925f
0xarchit <0xarchit@gmail.com>
2025-04-06 00:00:25 +0530

    feat: add loading spinner and handle loading state in PostList component

--- social-media-2/src/components/PostList.jsx
+++ social-media-2/src/components/PostList.jsx
@@ -1,27 +1,33 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, getPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
+
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      .then((data) => getPosts(data.posts));
+      .then((data) => {
+        getPosts(data.posts);
+        setFetching(false);
+      });
   }, []);
 
   return (
     <>
-      {postList.length === 0 && (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && (
         <div className="noposts d-grid justify-content-center">
           <div className="alert alert-warning" role="alert">
             No posts available. Please create a post first.
           </div>
         </div>
       )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
