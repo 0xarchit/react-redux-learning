d623f928d06e359d2e532a9cc2cbb03a60d2ec4d
0xarchit <0xarchit@gmail.com>
2025-04-05 23:04:51 +0530

    refactor: replace manual post fetching with useEffect for automatic data retrieval

--- social-media-2/src/components/PostList.jsx
+++ social-media-2/src/components/PostList.jsx
@@ -1,15 +1,14 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 
 const PostList = () => {
   const { postList, getPosts } = useContext(PostListData);
-
-  const onGetPostsClick = () => {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => getPosts(data.posts));
-  };
+  }, []);
 
   return (
     <>
@@ -18,13 +17,6 @@ const PostList = () => {
           <div className="alert alert-warning" role="alert">
             No posts available. Please create a post first.
           </div>
-          <button
-            type="button"
-            className="btn btn-primary btn-sm ms-2"
-            onClick={onGetPostsClick}
-          >
-            Refresh Latest Post From Server.
-          </button>
         </div>
       )}
       {postList.map((post) => (
